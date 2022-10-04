7c757884d91fc87168d2d9e3d243d27aa7eb7335
chungen04 <b09901027@ntu.edu.tw>
2022-10-04 12:41:10 +0800

    update gitignore

--- lab1/sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1663294143 /home/raid7_2/userb09/b09027/Digital-Circuit/lab1/src/Top.sv
-1663294143 /home/raid7_2/userb09/b09027/Digital-Circuit/lab1/sim/tb_Top.sv
-1663045687 /home/raid7_2/userb09/b09011/NTUEE-DCLAB-Materials/lab1/sim/tb_Top.sv
-1663253221 /home/raid7_2/userb09/b09011/NTUEE-DCLAB-Materials/lab1/src/Top_example.sv
--- lab2/src/tb_verilog/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,2 +0,0 @@
-1664719287 /home/raid7_2/userb09/b09027/Digital-Circuit/lab2/src/tb_verilog/tb.sv
-1664619813 /home/raid7_2/userb09/b09027/Digital-Circuit/lab2/src/Rsa256Core.sv
