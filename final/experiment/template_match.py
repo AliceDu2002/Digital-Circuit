29607fb0e17f1c700feb71f93cac540e6d45d030
AliceDu2002 <alicedu0211@gmail.com>
2022-12-06 13:50:35 +0800

    update image experiment

---
+++ final/experiment/template_match.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+img_rgb = cv.imread('img/ring_edge.jpg')
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+template = cv.imread('img/ring_edge_template.jpg',0)
+w, h = template.shape[::-1]
+res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+threshold = 0.25
+
+loc = np.where( res >= threshold)
+print(loc)
+for pt in zip(*loc[::-1]):
+    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+cv.imwrite('res.png',img_rgb)
