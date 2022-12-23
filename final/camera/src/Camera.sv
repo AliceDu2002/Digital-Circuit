9bef83871fdecc9cb42f337b435ee56f0e1a15ae
AliceDu2002 <alicedu0211@gmail.com>
2022-12-23 15:12:56 +0800

    camera.sv fixed

--- final/camera/src/Camera.sv
+++ final/camera/src/Camera.sv
@@ -16,7 +16,7 @@ module Camera (
     input[7:0] ov7670_data; // get RGB data
 
     // to SDRAM
-    output address; // length?
+    output[19:0] address; // length?
     inout[23:0] data; // a pixel
 
     // to VGA
@@ -48,6 +48,7 @@ logic address_r, address_w;// address
 logic count_byte_r, count_byte_w;
 logic[8:0] count_row_r, count_row_w;
 logic[9:0] count_col_r, count_col_w; 
+logic capture_r, capture_w;
 
 //=== output ===
 assign vga_red = rgb8_r[23:16];
@@ -68,6 +69,7 @@ always_comb begin
     count_byte_w = count_byte_r;
     count_row_w = count_row_r;
     count_col_w = count_col_r;
+    capture_w = capture_r;
     case(state_r)
         S_IDLE: begin
             initial_ready_w = 1;
@@ -84,11 +86,12 @@ always_comb begin
                 state_w = S_ROW;
                 address_w = 0;
             end
-            if(capture) begin
+            if(capture || capture_r) begin
                 state_w = S_CAPT;
             end
         end
         S_ROW: begin
+            if(capture) begin capture_w = 1; end
             if(!ov7670_vsync) begin
                 if(count_row_r < 480) begin
                     if(ov7670_href) begin
@@ -101,35 +104,46 @@ always_comb begin
             end
         end
         S_PIXEL: begin
-            if(count_col_r <= 639) begin
+            if(capture) begin capture_w = 1; end
+            if(count_col_r < 639) begin
+                if(!count_byte_r)   begin 
+                    rgb4_w[11:8] = ov7670_data[3:0];
+                    count_byte_w = count_byte_r + 1;  
+                end
+                else if(count_byte_r)    begin 
+                    count_byte_w = count_byte_r + 1;
+                    count_col_w = count_col_r + 1;
+                    address_w = address_r + 1;
+                    rgb8_w[23:20] = rgb4_r[11:8];
+                    rgb8_w[19:16] = 4'd0;
+                    rgb8_w[15:13] = ov7670_data[7:4];
+                    rgb8_w[12:8] = 4'd0;
+                    rgb8_w[7:4] = ov7670_data[3:0];
+                    rgb8_w[4:0] = 4'd0;
+                end
+                state_w = S_PIXEL;
+            end
+            else if(count_col_r == 639) begin
                 if(!count_byte_r)   begin 
                     rgb4_w[11:8] = ov7670_data[3:0];
                     count_byte_w = count_byte_r + 1;
                 end
                 if(count_byte_r)    begin 
-                    rgb4_w[7:0] = ov7670_data[7:0]; 
                     count_byte_w = count_byte_r + 1;
                     count_col_w = count_col_r + 1;
                     address_w = address_r + 1;
-                    state_w = S_RGB;
+                    rgb8_w[23:20] = rgb4_r[11:8];
+                    rgb8_w[19:16] = 4'd0;
+                    rgb8_w[15:13] = ov7670_data[7:4];
+                    rgb8_w[12:8] = 4'd0;
+                    rgb8_w[7:4] = ov7670_data[3:0];
+                    rgb8_w[4:0] = 4'd0;
                 end
-                state_w = S_PIXEL;
-            end
-            else if(count_col_r == 640) begin
                 if(!ov7670_href) begin
+                    count_row_w = count_row_r + 1;
                     state_w = S_ROW;
                 end
-                count_row_w = count_row_r + 1;
             end
-        end
-        S_RGB: begin
-            rgb8_w[23:20] = rgb4_r[11:8];
-            rgb8_w[19:16] = 4'd0;
-            rgb8_w[15:13] = rgb4_r[7:4];
-            rgb8_w[12:8] = 4'd0;
-            rgb8_w[7:4] = rgb4_r[3:0];
-            rgb8_w[4:0] = 4'd0;
-            state = S_PIXEL;
         end
         S_CAPT: begin
             if(nios_finish) begin
@@ -148,7 +162,8 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
         state_r <= 0;    
         count_byte_r <= 0;
         count_row_r <= 0;
-        count_col_r <= 0;   
+        count_col_r <= 0; 
+        capture_r <= 0;  
     end
     else begin
         rgb4_r <= rgb4_w;
@@ -159,6 +174,7 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
         count_byte_r <= count_byte_w;
         count_row_r <= count_row_w;
         count_col_r <= count_col_w;
+        capture_w <= 0;
     end
 end
 
