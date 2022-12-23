754ea22a330ce9ea55c65362f765ca5ffb0b26b4
AliceDu2002 <alicedu0211@gmail.com>
2022-12-23 11:43:36 +0800

    add blob detection hdl code gen by matlab

---
+++ final/BLOB_HDL/highlightRemovedDeadBlocks.m
@@ -0,0 +1,9 @@
+cs.HiliteType = 'user1';
+cs.ForegroundColor = 'black';
+cs.BackgroundColor = 'red';
+set_param(0, 'HiliteAncestorsData', cs);
+hilite_system('BlobAnalysisHDL/BlobDetector/CCA_Algorithm/TimingSubsystem/Bus Selector','user1');
+hilite_system('BlobAnalysisHDL/BlobDetector/CCA_Algorithm/TimingSubsystem/Logical Operator','user1');
+hilite_system('BlobAnalysisHDL/BlobDetector/CCA_Algorithm/gatheroutput/Switch4','user1');
+hilite_system('BlobAnalysisHDL/BlobDetector/CCA_Algorithm/Pixel Stream FIFO/InputSizeValidation','user1');
+hilite_system('BlobAnalysisHDL/BlobDetector/CCA_Algorithm/Pixel Stream FIFO/InputSizeValidation/noHDL','user1');
